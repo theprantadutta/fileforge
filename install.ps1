1f61be5ad8a64bc5c98fe5685c8d6a2b886cd197
PRANTA Dutta <pranta.dutta@kdsgroup.net>
2025-01-27 09:32:07 +0600

    added a new install script

---
+++ install.ps1
@@ -0,0 +1,71 @@
+# GitHub repo details
+$repoOwner = "theprantadutta"
+$repoName = "fileforge"
+
+# Headers for GitHub API
+$headers = @{
+    "User-Agent" = "Mozilla/5.0"
+}
+
+# Get the latest release version from GitHub API
+$latestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/$repoOwner/$repoName/releases/latest" -Headers $headers
+$version = $latestRelease.tag_name
+
+# Check if assets exist
+if ($latestRelease.assets -eq $null) {
+    Write-Host "No assets found in the latest release."
+    exit
+}
+
+# Find the download URL for the Windows version
+$windowsAssetUrl = $latestRelease.assets | Where-Object { $_.name -like "*windows.exe" } | Select-Object -ExpandProperty browser_download_url
+
+# Check if the Windows asset URL is found
+if (-Not $windowsAssetUrl) {
+    Write-Host "No Windows executable found in the release assets."
+    exit
+}
+
+# Define installation path in the user's directory
+$installDir = "$HOME\AppData\Local\Programs\$repoName"
+
+# Check if directory exists, create if not
+if (-Not (Test-Path $installDir)) {
+    New-Item -ItemType Directory -Path $installDir
+}
+
+# Download the binary for Windows
+$exePath = "$installDir\$repoName_v$version.exe"
+Write-Host "Downloading $repoName version $version for Windows..."
+try {
+    Invoke-WebRequest -Uri $windowsAssetUrl -OutFile $exePath -ErrorAction Stop
+} catch {
+    Write-Host "Error downloading the executable: $_"
+    exit
+}
+
+# Rename the downloaded file to "$repoName.exe" for consistency
+$finalExePath = "$installDir\$repoName.exe"
+if (Test-Path $exePath) {
+    # Rename the downloaded file to "$repoName.exe" for consistency
+    $finalExePath = "$installDir\$repoName.exe"
+    if (Test-Path $finalExePath) {
+        Write-Host "Existing $repoName.exe found. Deleting..."
+        Remove-Item -Path $finalExePath -Force
+    }
+    Rename-Item -Path $exePath -NewName $finalExePath
+} else {
+    Write-Host "Downloaded file not found. Something went wrong."
+    exit
+}
+
+# Add to user's PATH permanently
+$envPath = [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User)
+if (-not $envPath.Split(";").Contains($installDir)) {
+    [Environment]::SetEnvironmentVariable("Path", "$envPath;$installDir", [EnvironmentVariableTarget]::User)
+    Write-Host "Added $installDir to the user's PATH."
+} else {
+    Write-Host "$installDir is already in the user's PATH."
+}
+
+Write-Host "$repoName v$version installed successfully in $installDir!"
